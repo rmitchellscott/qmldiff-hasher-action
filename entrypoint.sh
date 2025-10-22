26713c344f93df032020bc2fbb2149af8fe1df9d
Mitchell Scott <10804314+rmitchellscott@users.noreply.github.com>
2025-10-21 20:17:09 -0600

    add custom commit message, publish docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 # ENV: INPUT_QMD_HASH_RULES
 # ENV: INPUT_DEST_REPO_NAME
 # ENV: INPUT_REPO_ACCESS_TOKEN
+# ENV: INPUT_COMMIT_MESSAGE (optional)
 
 set -eo pipefail
 
@@ -31,6 +32,11 @@ if [ -z "$(git diff --cached)" ]; then
 	exit 0
 fi
 
-git commit -m "Pull state from private repo"
+if [ -z "$INPUT_COMMIT_MESSAGE" ]; then
+	COMMIT_MSG="Update hashed files - $(date -u +"%Y-%m-%d %H:%M:%S UTC")"
+else
+	COMMIT_MSG="$INPUT_COMMIT_MESSAGE"
+fi
+git commit -m "$COMMIT_MSG"
 git remote set-url origin "https://a:$INPUT_REPO_ACCESS_TOKEN@github.com/$INPUT_DEST_REPO_NAME.git"
 git push origin $(git rev-parse --abbrev-ref HEAD)
